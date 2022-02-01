3698c5a3eb083f464c4f377da0681758a054fe5a
Connor Earl <connorearl77@gmail.com>
2022-01-31 23:37:09 -0700

    Bed pid temps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -649,9 +649,9 @@
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
   // Creality Ender-3 Pro
-  #define DEFAULT_bedKp 50.71
-  #define DEFAULT_bedKi 9.88
-  #define DEFAULT_bedKd 173.43
+  #define DEFAULT_bedKp 77.05
+  #define DEFAULT_bedKi 15.05
+  #define DEFAULT_bedKd 263.01
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
